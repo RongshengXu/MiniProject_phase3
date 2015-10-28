76530254c05063779b2f7281ce6d1659ed326968
Rongsheng Xu <xurongsheng2010@gmail.com>
2015-10-28 10:37:52 -0500

    camera preview

--- Android/app/src/main/java/apt/com/miniproject/TakePicture.java
+++ Android/app/src/main/java/apt/com/miniproject/TakePicture.java
@@ -11,12 +11,21 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.apache.http.Header;
+
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -66,7 +75,7 @@ public class TakePicture extends ActionBarActivity implements
         takePhotoButton.setEnabled(true);
         takePhotoButton.setClickable(true);
 
-        submitButton = (Button) findViewById(R.id.submit_button);
+        submitButton = (Button) findViewById(R.id.upload_to_server);
         submitButton.setOnClickListener(listener);
         submitButton.setEnabled(false);
         submitButton.setClickable(false);
@@ -86,16 +95,48 @@ public class TakePicture extends ActionBarActivity implements
                 //System.out.println("take_photo_button_clicked");
                 mCamera.takePicture(null,null,this);
                 break;
-            case R.id.submit_button:
-                Intent intent= new Intent(this, ImageUpload.class);
-                intent.putExtra("message", streamName);
-                intent.putExtra("user", user_name);
-                intent.putExtra("id", "camera");
-                intent.putExtra("data", image_data);
-                //startActivity(intent);
-                startActivityForResult(intent, TAKE_PICTURE);
+//            case R.id.submit_button:
+//                Intent intent= new Intent(this, ImageUpload.class);
+//                intent.putExtra("message", streamName);
+//                intent.putExtra("user", user_name);
+//                intent.putExtra("id", "camera");
+//                intent.putExtra("data", image_data);
+//                //startActivity(intent);
+//                startActivityForResult(intent, TAKE_PICTURE);
                 //uploadImage();
+            case R.id.upload_to_server:
+                uploadHandler(image_data, streamName);
+                break;
         }
+    }
+
+    private void uploadHandler(final byte[] encodedImage, final String stream_name) {
+        AsyncHttpClient httpClient = new AsyncHttpClient();
+        String upload_url = "http://sacred-highway-108321.appspot.com/android/upload";
+        RequestParams params = new RequestParams();
+        params.put("file",new ByteArrayInputStream(encodedImage));
+        params.put("streamname", stream_name);
+        AsyncHttpClient client = new AsyncHttpClient();
+        System.out.println(upload_url+" \n" + encodedImage.toString());
+        client.post(upload_url, params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                Log.w("async", "success!!!!");
+                Toast.makeText(context, "Upload Successful", Toast.LENGTH_SHORT).show();
+                // go back to single stream view
+                Intent intent= new Intent(context, ViewStreamSingle.class);
+                intent.putExtra("message", stream_name);
+                intent.putExtra("user", user_name);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                System.out.println(new String(errorResponse));
+                Log.e("Posting_to_blob", "There was a problem in retrieving the url : " + e.toString());
+            }
+        });
+//            
     }
 
     public void surfaceCreated(SurfaceHolder holder) {
